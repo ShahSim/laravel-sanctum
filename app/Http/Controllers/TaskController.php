0a6e156d5829aa8d71753d0427202fad154af983
Shahdeen Walid Simion <simion@techknowgram.com>
2022-11-13 17:37:26 +0600

    13/11/22

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -24,7 +25,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return response()->json("Test");
+        return TaskResource::collection(Task::paginate(10));
     }
 
     /**
